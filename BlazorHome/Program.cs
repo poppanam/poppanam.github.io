fcac5be926546f214df8c8d768344fb6ca27e538
Erik Maenpaa <eggmaenpaa@yahoo.com>
2024-12-18 12:54:48 -0500

    Remove old maui stuff and add blazor

---
+++ BlazorHome/Program.cs
@@ -0,0 +1,39 @@
+using BlazorHome.Components;
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+
+    Path.Combine(Directory.GetCurrentDirectory(), "StaticFiles" )),
+
+    RequestPath = "/StaticFiles"
+    });
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
